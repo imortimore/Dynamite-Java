8133b7b92dd9d7f53bdc5c32c99ff244bbcb111f
imortimore <isaac.mortimore@internet.com>
2022-09-30 14:23:22 +0100

    working but still bad :(

--- src/training/dynamite/MyBot.java
+++ src/training/dynamite/MyBot.java
@@ -23,18 +23,24 @@ public class MyBot implements Bot {
         //logic with Dynamite present
         while (DynamiteUsed >= 100) {
             //if game is started start with a D
-            if (gamestate.getRounds().size() == 0) {
+            if (gamestate.getRounds().size() >= 4) {
                 DynamiteUsed++;
                 return Move.D;
-            } else if () {
-            } else if () {
             } else {
+                Round theirMove1Ago = (Round) gamestate.getRounds().get(gamestate.getRounds().size() - 1);
+                Round theirMove2Ago = (Round) gamestate.getRounds().get(gamestate.getRounds().size() - 2);
+                Round theirMove4Ago = (Round) gamestate.getRounds().get(gamestate.getRounds().size() - 4);
+                if (theirMove1Ago == theirMove2Ago) {
+                    return this.getCounterMove(theirMove1Ago.getP1());
+                } else if (theirMove2Ago == theirMove4Ago) {
+                    return this.getCounterMove(theirMove1Ago.getP1());
+                } else {
+                    DynamiteUsed++;
+                    return Move.D;
             }
         }
-        int randomNumberBetween0And3 = (int) Math.floor(Math.random() * 3.0);
-        Move[] possibleMoves = new Move[]{Move.R, Move.P, Move.S};
-        Move randomMove = possibleMoves[randomNumberBetween0And3];
-        return randomMove;
+    }
+        return this.getRandomMove();
     }
 
     private Move getCounterMove(Move theirLastMove) {
@@ -57,60 +63,15 @@ public class MyBot implements Bot {
     //test logic for this method
     //Round theirMove2Ago = (Round) gamestate.getRounds().get(gamestate.getRounds().size() - 2);
     //Round theirMove4Ago = (Round) gamestate.getRounds().get(gamestate.getRounds().size() - 4);
-    private Move Pattern010(Move theirMove2Ago, Move theirMove4Ago){
-        switch (theirMove2Ago) {
-            case R:
-                return Move.P;
-            case P:
-                return Move.S;
-            case S:
-                return Move.R;
-            case D:
-                return Move.W;
-            case W:
-                return Move.R;
-            default:
-                throw new RuntimeException("Invalid last move from P1");
-        }
-    }
-    private Move Pattern000(Move theirMove1Ago, Move theirMove2Ago){
-        switch (theirMove2Ago) {
-            case R:
-                return Move.P;
-            case P:
-                return Move.S;
-            case S:
-                return Move.R;
-            case D:
-                return Move.W;
-            case W:
-                return Move.R;
-            default:
-                throw new RuntimeException("Invalid last move from P1");
-        }
+
+    public Move getRandomMove() {
+        int randomNumberBetween0And3 = (int) Math.floor(Math.random() * 3.0);
+        Move[] possibleMoves = new Move[]{Move.R, Move.P, Move.S};
+        Move randomMove = possibleMoves[randomNumberBetween0And3];
+        return randomMove;
     }
 }
-
-
-
-
-
-
 
 //Round theirMove1Ago = (Round) gamestate.getRounds().get(gamestate.getRounds().size() - 1);
 //return this.getCounterMove(theirMove1Ago.getP1());
 
-
-//        while (DynamiteUsed >= 100){
-//            if (gamestate.getRounds().size() == 0) {
-//                DynamiteUsed ++;
-//                return Move.D;
-//            }else {
-//                    //random move incl dynamites
-//                    int randomNumberBetween0And4 = (int)Math.floor(Math.random() * 4.0);
-//                    Move[] possibleMoves = new Move[]{Move.R, Move.P, Move.S, Move.D};
-//                    Move randomMoveWithD = possibleMoves[randomNumberBetween0And4];
-//                    DynamiteUsed ++;
-//                    return randomMoveWithD;
-//            }
-//        }
